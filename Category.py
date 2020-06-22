c67afd4093d89eabea8585ccfdbfe9d76bb97364
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-22 19:56:18 +0700

    add columns and init in category

--- Category.py
+++ Category.py
@@ -1,8 +1,15 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 
+from TypeFlow import TypeFlow
 from base import Base
 
 
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(Integer)
+
+    def __init__(self, name: str, type_flow: TypeFlow):
+        self.name = name
+        self.type = type_flow
