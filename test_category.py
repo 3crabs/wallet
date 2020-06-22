6ee73b43bde61db40a75ed3bc3848fb1ddb4c831
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-22 18:23:09 +0700

    add setUp and test add in base

--- test_category.py
+++ test_category.py
@@ -1,9 +1,19 @@
 import unittest
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from base import Base
+from Category import Category
 from answer import text_answer
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        Base.metadata.create_all(engine)
+        self.session = sessionmaker(bind=engine)()
 
     def test_add_category_transport(self):
         answer = text_answer('Wallet добавь категорию расходов транспорт')
@@ -12,6 +22,12 @@ class MyTestCase(unittest.TestCase):
     def test_add_category_work(self):
         answer = text_answer('Wallet добавь категорию доходов работа')
         self.assertEqual('Категория работа добавлена!', answer)
+
+    def test_add_category_transport_in_base(self):
+        text_answer('Wallet добавь категорию расходов транспорт')
+        category = self.session.query(Category).all().ftrst()
+        self.assertEqual('транспорт', category.name)
+        self.assertFalse(category.is_profit)
 
 
 if __name__ == '__main__':
