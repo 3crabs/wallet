f0487d0706cb6f10c6237fda9419178254153007
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-22 19:57:52 +0700

    add save in data base

--- answer.py
+++ answer.py
@@ -1,3 +1,11 @@
+from Category import Category
+from Database import Database
+from TypeFlow import TypeFlow
+
+
 def text_answer(text: str):
     category_name = text.split(' ')[4]
+    session = Database.get_instance().session()
+    session.add(Category(category_name, TypeFlow.PROFIT))
+    session.commit()
     return f'Категория {category_name} добавлена!'
